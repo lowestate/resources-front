1174b49131c79f9844af69dbd3e3a27d61564e62
Yan <bents.yan1@gmail.com>
2023-11-09 15:51:44 +0300

    Updates

--- modules/ds.ts
+++
@@ -1,9 +0,0 @@
-export interface Resource {
-    ID: number,
-    ResourceName: string,
-    IsAvailable: boolean,
-    Place: string,
-    Image: string,
-    Month: string,
-    MonthlyProduction: number,
-}
